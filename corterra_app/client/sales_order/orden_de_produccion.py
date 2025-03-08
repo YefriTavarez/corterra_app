f80b7a0113e3f6e7eeec41b3eeaab0980748fd2c
Yefri Tavarez <yefritavarez@gmail.com>
2025-03-07 23:29:57 -0500

    feat: Enhance Delivery Note creation by linking to associated Sales Order

--- corterra_app/client/sales_order/orden_de_produccion.py
+++ corterra_app/client/sales_order/orden_de_produccion.py
@@ -49,7 +49,35 @@ def make_sales_invoice(production_order_id: str):
 
 @frappe.whitelist()
 def make_delivery_note(production_order_id: str):
-	...
+	# to create the Delivery Note, we need to get the related Sales Order
+	# and then use the ERPNext method they use to create the Delivery Note
+	prod = get_production_order(production_order_id)
+
+	if not prod.sales_order:
+		frappe.throw("No se ha asociado una Orden de Venta a esta Orden de Producción")
+
+	dn = sales_order.make_delivery_note(
+		source_name=prod.sales_order,
+		target_doc=None,
+		ignore_permissions=True,
+	)
+
+	if dn.is_new():
+		dn.insert()
+
+	try:
+		dn.submit()
+	except Exception as e:
+		frappe.log_error()
+		return {
+			"ok": False,
+			"error": str(e),
+		}
+
+	return {
+		"ok": True,
+		"id": dn.name,
+	}
 
 
 def get_production_order(name: str):
